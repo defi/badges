db78c3f43be2b7bad8de16417d6e93724971921d
Ian Macalinao <me@ian.pw>
2018-11-19 16:06:00 -0800

    read from blockchain

--- scripts/mint_badge.js
+++ scripts/mint_badge.js
@@ -18,22 +18,42 @@ const Bluebird = require("bluebird");
 
 module.exports = async cb => {
   const badgeContract = await DeFiBadge.deployed();
+
+  // read from blockchain
+  const totalSupply = await badgeContract.totalSupply();
+  console.log("Total supply", totalSupply.toNumber());
+  const res = await Bluebird.map(
+    Array(totalSupply.toNumber())
+      .fill(null)
+      .map((_, idx) => idx),
+    async tokenId => {
+      console.log(tokenId);
+      return await badgeContract.ownerOf(tokenId);
+    },
+    {
+      concurrency: 10
+    }
+  );
+  const res2 = res.map(x => x.toLowerCase());
+
   console.log("Minting...");
   const filtered = attendees.filter(
-    x => !attendeesDone.includes(x.toLowerCase())
+    x =>
+      !attendeesDone.includes(x.toLowerCase()) &&
+      !res2.includes(x.toLowerCase())
   );
   console.log(
-    `Found ${attendeesDone.length} attendees, so only ${
-      filtered.length
-    } (previous ${attendees.length})`
+    `Found ${attendeesDone.length} attendees and ${
+      res2.length
+    } attendees, so only ${filtered.length} (previous ${attendees.length})`
   );
   await Bluebird.map(
     filtered,
     async attendee => {
       // retries
-      for (let i = 0; i < 3; i++) {
+      for (let i = 0; i < 5; i++) {
         try {
-          console.log(`Mint for ${attendee}`);
+          console.log(`Mint for ${attendee} (attempt ${i})`);
           const tx = await badgeContract.mint(attendee, BADGE_URL);
           console.log("Finished tx", tx);
           return;
